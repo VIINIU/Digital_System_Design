cb8e2fd1b8df918d51b6282fa94501cff559cb88
viiniu <11e26db@gmail.com>
2026-05-08 17:54:04 +0900

    12

--- tb_traffic_emergency.v
+++ tb_traffic_emergency.v
@@ -26,25 +26,26 @@ module tb_traffic_emergency;
   forever #1 Clk = ~Clk;
  end
  
- initial begin
-  C = 0;
-  Emergency = 0;
-  reset = 1;
-  #10;
-  reset = 0;
-  #20;
-  C = 1;
-  #20;
-  C = 0;
-  #30;
-  C = 1;
-  #80;   
-  C = 0;
-  #40;
-  Emergency = 1;
-  #20;
-  Emergency = 0;
-  #100;  
-  $finish;
+ iinitial begin
+    C = 0;
+    Emergency = 0;
+    reset = 1;
+    #10;        // t=10
+    reset = 0;
+    #20;        // t=30
+    C = 1;
+    #20;        // t=50
+    C = 0;
+    #30;        // t=80
+    C = 1;
+    #80;        // t=160
+    C = 0;
+    #40;        // t=200
+    Emergency = 1;
+    #30;        // t=230
+    Emergency = 0;
+    
+    #100;       // t=330, 충분한 관찰 시간
+    $finish;
  end
 endmodule
